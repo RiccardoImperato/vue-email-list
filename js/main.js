00cd387532707f612563543ce7b4b84a6ab93f27
RiccardoImperato <riccardo.imperato@libero.it>
2023-12-05 16:51:29 +0100

    feat: lista email in pagina

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            emailList: [],
+        }
+    },
+    methods: {},
+    created() {
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+                response.data.response;
+                this.emailList.push(response.data.response);
+            })
+        }
+        return this.emailList;
+    },
+}).mount('#app');
